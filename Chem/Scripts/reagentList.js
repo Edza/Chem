0845c40c8aa3bdeef82f4a4e94a1b070c853008d
edza <edexdev@gmail.com>
2015-01-07 23:00:37 +0200

    work on combo boxes and js

--- Chem/Scripts/reagentList.js
+++ Chem/Scripts/reagentList.js
@@ -1,5 +1,14 @@
-﻿var x;
-$(function () {
-	alert("test");
-	x = "lol";
+﻿$(function () {
+    $.get("../reagent/list", function (data) {
+        var options = $("#reagentsSelect");
+        options.find('option').remove();
+        $.each(data, function () {
+            options.append($("<option />").val(this.ID).text(this.Name));
+        });
+    });
+
+    $("#addReagent").click(function () {
+        $("#reagents").val($("#reagents").val() + "\r\n" + $("#reagentsSelect option:selected").val());
+        $("#reagentsDisplayNames").text($("#reagentsDisplayNames").text() + $("#reagentsSelect option:selected").text());
+    });
 });
